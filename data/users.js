f96620a989df9df88954fe28c649afe15a1ac09a
sobatners <sobatners@gmail.com>
2026-06-17 09:23:09 +0700

    Add files via upload

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const USERS=[
+  {u:'admin',p:'ners2024',d:'Administrator'},
+  {u:'pengguna',p:'ujibeta123',d:'Pengguna Beta 1'},
+  {u:'ners2',p:'ners456',d:'Ners Kedua'},
+  {u:'mahasiswa',p:'mhs2024',d:'Mahasiswa Keperawatan'},
+  {u:'dosen',p:'dosen2024',d:'Dosen Pembimbing'}
+];
+
